d8f4618af0c3a254d3c0c48b912633b977c9e2dd
rodrics14 <srodri72003@gmail.com>
2023-12-01 00:23:10 -0600

    cambiamos el port

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const app= express();
-
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
 app.use(cors());
 
-app.set("port", 3001);
+app.set("port", PORT);
 
 module.exports=app;
